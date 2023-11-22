0d9592ed37d92e24aace49fc2da60c7a26fcd834
Sakura <3420757701@qq.com>
2023-11-23 01:54:36 +0800

    修改部分ui添加按钮形状

--- app/src/main/java/com/example/smartcity_20/service/house/HouseActivity.kt
+++ app/src/main/java/com/example/smartcity_20/service/house/HouseActivity.kt
@@ -3,10 +3,13 @@ package com.example.smartcity_20.service.house
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.smartcity_20.R
+import com.example.smartcity_20.config.kotlin.viewBinding
+import com.example.smartcity_20.databinding.ActivityHouseBinding
 
 class HouseActivity : AppCompatActivity() {
+    private val vb by viewBinding(ActivityHouseBinding::inflate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_house)
+        setContentView(vb.root)
     }
 }
